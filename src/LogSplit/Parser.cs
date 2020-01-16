6729ad9b2aa9699f0caa02ee18da89bf6742f9b1
Christian Walpen <christianwalpen@opacc.ch>
2020-01-16 10:30:16 +0100

    Initial commit

---
+++ src/LogSplit/Parser.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace LogSplit
+{
+    public class Parser
+    {
+        private readonly string _pattern;
+
+        public Parser(string pattern)
+        {
+            _pattern = pattern;
+        }
+
+        public ParserResult Parse(string value)
+        {
+            var splits = SplitPattern(_pattern);
+            var items = new List<Token>();
+
+            foreach (var scan in splits)
+            {
+                var separatorLength = scan.Separator != null ? scan.Separator.Length : 0;
+                var index = scan.Separator != null ? value.IndexOf(scan.Separator, StringComparison.OrdinalIgnoreCase) + separatorLength : -1;
+                if (index < 0)
+                {
+                    index = value.IndexOf(" ", StringComparison.OrdinalIgnoreCase);
+                }
+
+                if (scan.Placeholder != null)
+                {
+
+                    var key = scan.Placeholder.Substring(2, scan.Placeholder.Length - 3);
+
+                    items.Add(new Token(key, value.Substring(0, index - separatorLength).Trim()));
+                }
+
+
+                value = value.Substring(index);
+            }
+
+            return new ParserResult(items);
+        }
+
+        private IEnumerable<Scan> SplitPattern(string pattern)
+        {
+            var splits = new List<Scan>();
+            var split = ScanNext(pattern);
+            splits.Add(split);
+
+            while (split.Separator != null)
+            {
+                var length = pattern.StartIndex(split);
+
+                var splitlenth = split.Separator != null ? pattern.IndexOf(split.Separator, length, StringComparison.OrdinalIgnoreCase) + split.Separator.Length : 0;
+                pattern = pattern.Substring(splitlenth);
+
+                split = ScanNext(pattern);
+                if (!string.IsNullOrEmpty(split.Placeholder))
+                {
+                    splits.Add(split);
+                }
+            }
+
+            return splits;
+        }
+
+        private Scan ScanNext(string pattern)
+        {
+            var start = pattern.IndexOf("%{", 0, StringComparison.OrdinalIgnoreCase);
+            if (start > 0)
+            {
+                return new Scan(null, pattern.Substring(0, start));
+            }
+
+            var index = pattern.IndexOf("}", StringComparison.OrdinalIgnoreCase);
+            if (index < 0)
+            {
+                return new Scan(pattern, null);
+            }
+
+
+            var value = pattern.Substring(0, index + 1);
+            var nextStart = pattern.IndexOf("%{", index, StringComparison.OrdinalIgnoreCase);
+            if (nextStart < 0)
+            {
+                nextStart = pattern.Length;
+            }
+
+            var next = value.Length < nextStart ? pattern.Substring(value.Length, nextStart - value.Length) : null;
+            return new Scan(value, next);
+        }
+    }
+
+    internal static class StringExtensions
+    {
+        public static int StartIndex(this string pattern, Scan scan)
+        {
+            var placeholder = scan.Placeholder ?? string.Empty;
+            var start = pattern.IndexOf(placeholder, StringComparison.OrdinalIgnoreCase);
+            return start + placeholder.Length;
+        }
+    }
+
+    public class Scan
+    {
+        public Scan(string placeholder, string separator)
+        {
+            Placeholder = placeholder;
+            Separator = separator;
+        }
+
+        public string Separator { get; set; }
+
+        public string Placeholder { get; set; }
+    }
+
+    public sealed class ParserResult : ReadOnlyCollection<Token>
+    {
+        public ParserResult(IList<Token> items)
+            : base(items ?? new List<Token>())
+        {
+        }
+    }
+
+    public sealed class Token
+    {
+        public Token(string key, object value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public string Key { get; }
+        public object Value { get; }
+    }
+}
---
+++ src/Tests/LogSplit.Tests/ParserTests.cs
@@ -0,0 +1,83 @@
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace LogSplit.Tests
+{
+    public class ParserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Parser_1()
+        {
+            var str = @"2020-01-14 21:15:41.4079 INFO  [PC-NAME] [PC-NAME\iis.service] [5640:management.tool.agent.exe] [Opacc.ServiceBus.Client.Common.Program.BaseServiceBusClientWinService] [Thr5] Startup delay: 3 sec remaining";
+            //str = @"2020-01-15 10:05:50.1545 ERROR [PC-NAME] [PC-NAME\iis.service] [6128:management.tool.agent.exe] [management.tool.Agent.CacheManagerCore] [Thr11] Maintain failed: ReloadOOWebPortalConfigAndInitiateActivities failed: Creation of ServiceBus-Connection";
+
+            //var pattern = "%{TIMESTAMP:timestamp} %{WORD:time} %{WORD:level}  [%{WORD:client}] [%{WORD:user}] [%{WORD:agent}] [%{WORD:source}] [%{WORD:thread}] %{GREEDYDATA:message}";
+            //var pattern = "%{TIMESTAMP:timestamp} %{GREEDYDATA:message}";
+            //var pattern = "%{YEAR:year}-%{MONTHNUM:month}-%{MONTHDAY:day}";
+            var pattern = "%{year}-%{month}-%{day}";
+
+            var parser = new Parser(pattern);
+            var result = parser.Parse(str);
+
+            result[0].Should().BeEquivalentTo(new {Key = "year", Value = "2020"});
+            result[1].Should().BeEquivalentTo(new { Key = "month", Value = "01" });
+            result[2].Should().BeEquivalentTo(new { Key = "day", Value = "14" });
+        }
+
+        [Test]
+        public void Parser_2()
+        {
+            var str = @"2020-01-14 21:15:41.4079 INFO  [PC-NAME] [PC-NAME\iis.service] [5640:management.tool.agent.exe] [Opacc.ServiceBus.Client.Common.Program.BaseServiceBusClientWinService] [Thr5] Startup delay: 3 sec remaining";
+            
+            var pattern = "%{date} %{time} %{level}";
+
+            var parser = new Parser(pattern);
+            var result = parser.Parse(str);
+
+            result.Count.Should().Be(3);
+
+            result[0].Should().BeEquivalentTo(new { Key = "date", Value = "2020-01-14" });
+            result[1].Should().BeEquivalentTo(new { Key = "time", Value = "21:15:41.4079" });
+            result[2].Should().BeEquivalentTo(new { Key = "level", Value = "INFO" });
+        }
+
+        [Test]
+        public void Parser_Splitter_WithBrackets()
+        {
+            var str = @"[2020-01-14 21:15:41.4079] [INFO]  [PC-NAME] [PC-NAME\iis.service] [5640:management.tool.agent.exe] [Opacc.ServiceBus.Client.Common.Program.BaseServiceBusClientWinService] [Thr5] Startup delay: 3 sec remaining";
+
+            var pattern = "[%{date}] [%{level}]";
+
+            var parser = new Parser(pattern);
+            var result = parser.Parse(str);
+
+            result.Count.Should().Be(2);
+
+            result[0].Should().BeEquivalentTo(new { Key = "date", Value = "2020-01-14 21:15:41.4079" });
+            result[1].Should().BeEquivalentTo(new { Key = "level", Value = "INFO" });
+        }
+
+        [Test]
+        public void Parser_Splitter_NotAtEnd()
+        {
+            var str = @"2020-01-14 21:15:41.4079 INFO  [PC-NAME] [PC-NAME\iis.service] [5640:management.tool.agent.exe] [Opacc.ServiceBus.Client.Common.Program.BaseServiceBusClientWinService] [Thr5] Startup delay: 3 sec remaining";
+
+            var pattern = "%{date} %{time} %{level} [%{pc}]";
+
+            var parser = new Parser(pattern);
+            var result = parser.Parse(str);
+
+            result.Count.Should().Be(4);
+
+            result[0].Should().BeEquivalentTo(new { Key = "date", Value = "2020-01-14" });
+            result[1].Should().BeEquivalentTo(new { Key = "time", Value = "21:15:41.4079" });
+            result[2].Should().BeEquivalentTo(new { Key = "level", Value = "INFO" });
+            result[3].Should().BeEquivalentTo(new { Key = "pc", Value = "PC-NAME" });
+        }
+    }
+}
