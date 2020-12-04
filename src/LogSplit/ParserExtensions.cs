d9e4c69d57b363b45933cb4d7cdd69d3040c4c51
Christian Walpen <christianwalpen@opacc.ch>
2020-12-04 22:28:32 +0100

    String extension for direct parsing

---
+++ src/LogSplit/ParserExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace LogSplit
+{
+	public static class ParserExtensions
+	{
+		/// <summary>
+		/// Parse a logstring and extract the desired properties
+		/// </summary>
+		/// <param name="log">the logmessage</param>
+		/// <param name="pattern">the parserpattern</param>
+		/// <returns></returns>
+		public static ParserResult ParseLog(this string log, string pattern)
+		{
+			var parser = new Parser(pattern);
+			return parser.Parse(log);
+		}
+	}
+}
---
+++ src/Tests/LogSplit.Tests/ParserExtensionsTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LogSplit.Tests
+{
+	public class ParserExtensionsTests
+	{
+		[Test]
+		public void ParserExtensions_ParseLog()
+		{
+			var result = "01.01.2020 [INFO] [PC-NAME] The log message".ParseLog("%{date} [%{level}] [%{pc}] %{message:len(*)}");
+			result[0].Should().BeEquivalentTo(new { Key = "date", Value = "01.01.2020" });
+			result["date"].Should().Be("01.01.2020");
+		}
+	}
+}
