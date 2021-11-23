73268c0bcff322e76858fcae898625639bdf8885
Christian Walpen <christianwalpen@opacc.ch>
2021-11-23 13:58:53 +0100

    Test for unmatching pattern

---
+++ src/Tests/LogSplit.Tests/Api/PaternUnmatchTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace LogSplit.Tests.Api
+{
+    public class PaternUnmatchTests
+    {
+        [Test]
+        public void Api_PatternUnmatch()
+        {
+            var log = $"[{DateTime.Now:o}] [Information] The message";
+            var pattern = "[%{Date}] [%{Level}] [%{Message:len(*)}]";
+
+            var parser = new Parser(pattern);
+            var result = parser.Parse(log);
+
+            result.Should().Contain(r => r.Key == "Date").And.Contain(r => r.Key == "Level").And.Contain(r => r.Key == "Message");
+        }
+    }
+}
